9d729da5ada86b6c8340480fc4d571900d5bef9c
bramp <brampel.pel@gmail.com>
2019-06-18 13:17:42 +0200

    moved drafts to draft file

--- WiP.py
+++ WiP.py
@@ -88,3 +88,77 @@ def application(abstraction, variables):
         for c in range(0, len(newterm)):
             newterm_string += str(newterm[c])
         return newterm_string
+    
+'''
+numbers:
+    0 == (λsz.z)
+    1 == (λsz.s(sz))
+    2 == (λsz.s(s(sz)))
+
+succession:
+    Name: S
+    Profile: (λxyz.y(xyz))
+    inputs: m
+    outputs: n = m+1
+    Usage: Sm
+
+Addition:
+    Name: A
+    Profile: S == (λxyz.y(xyz))
+    inputs: a, b
+    outputs: c = a+b
+    Usage: aAb
+
+Multiplication:
+    Name: M
+    Profile: (λxyz.x(yz))
+    inputs: a,b
+    outputs: c = a*b
+    Usage: Mab
+    
+Predecessor:
+    Name: P
+    Profile: ((λn.nQ(λz.z00))F)1
+    where
+        Name: Q
+        Profile: Profile: (λpz.z(S(pT))(pT))
+        inputs: (a, b)
+        outputs: (S(a), b)
+        usage: Q(a,b)
+    and F will be defined down below
+    inputs: N, where N is a natural number
+    outputs: N-1 
+    Usage: PN
+
+Subtraction:
+    Name: B
+    Profile: (λxy.yPx)
+    inputs: m, n
+    outputs: l = m - n
+    Usage: Bmn
+
+Booleans:
+    True: T == (λsz.s)
+    False: F == 0 == (λsz.z)
+    
+AND operator:
+    Name: ∧
+    Profile: (λxy.xy(λuv.v)) == λxy.xyF
+    inputs: M, N
+    outputs: M ∧ N == (T or F)
+    Usage: ∧MN
+    
+OR operator:
+    Name: ∨
+    Profile: (λxy.x(λuv.u)y) == λxy.xTy
+    inputs: M, N
+    outputs: M ∨ N == (T or F)
+    Usage: ∨MN
+
+Negation:
+    Name: ¬
+    Profile: (λx.x(λuv.v)(λab.a)) == λx.xFT
+    inputs: M
+    outputs: N == negation of M
+    Usage: ¬ M
+'''
