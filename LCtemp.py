6348ef5f426797365b0222259ee3eeaffd9b3578
bramp <brampel.pel@gmail.com>
2019-06-20 22:51:53 +0200

    added reduce method for Applications. Can evaluate applications of any combination of functions and/or Variables. yet to expand to Applications of Applications. Also added a function call method for Abstractions, which works just fine but raises some other issues.

--- LCtemp.py
+++ LCtemp.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+#Issues: Calling An abstraction leaves the Abstraction itself permanently changed; for Id=Application(Variable('a'), Variable('a')) 
+#and x=Variable('x'), calculating Id(x) gives Variable('x'), which is fair enough,
+# but then printing Id gives Abstraction(Variable('a'), Variable('x')), which is nothing like the original function.
 
 class LambdaTerm:
     """Abstract Base Class for lambda terms."""
@@ -12,15 +15,14 @@ class LambdaTerm:
         """Substitute values for keys where they occur."""
         #let rules always be given in format [a, b] where a is the variable that should be replaced by variable b.
         raise NotImplementedError
-    def reduce(self):
-        """Beta-reduce."""
-        raise NotImplementedError
+   # def reduce(self):
+    #    """Beta-reduce."""
+     #   raise NotImplementedError
 
 class Variable(LambdaTerm):
     """Represents a variable."""
     def __init__(self, symbol):
         self.symbol = symbol
-        self.allargs = True
     def __repr__(self):
         return 'Variable('+repr(self.symbol)+')'
     def __str__(self):
@@ -37,13 +39,14 @@ class Abstraction(LambdaTerm):
     def __init__(self, variable, body): #alpha conversie!
         self.variable = variable
         self.body = body
-        self.allargs = False
+        self.rewind = self
     def __repr__(self):
         return "Abstraction("+repr(self.variable)+', '+repr(self.body)+')'
     def __str__(self):
         return '(λ'+str(self.variable)+'.'+str(self.body)+')'
-    def __call__(self, argument): raise NotImplementedError
-        
+    def __call__(self, argument):
+        copy = self
+        return Application(copy, argument).reduce()
     def substitute(self, rules): #given new variable should never collide with bound variable! also, we assume that variable in self.variable is immutable
         self.body = self.body.substitute(rules)
         return self
@@ -54,21 +57,16 @@ class Application(LambdaTerm):
     def __init__(self, function, argument): #implementeer Alfa-conversie om name collisions te voorkomen
         self.function = function
         self.argument = argument
-        if type(self.function) == type(self.argument) == Variable:
-            self.allargs = True
-        else:
-            self.allargs = False
     def __repr__(self):
         return 'Application('+repr(self.function)+', '+repr(self.argument)+')'
     def __str__(self):
-        if self.allargs:
-            return str(self.function)+str(self.argument)
-        elif self.function.allargs and self.argument.allargs:
-            return str(self.function)+str(self.argument)
-        else:
-            return '('+str(self.function)+')'+ str(self.argument)
+        return str(self.function)+str(self.argument)
     def substitute(self, rules):
         self.function.substitute(rules)
         self.argument.substitute(rules)
         return self
-    def reduce(self): raise NotImplementedError
+    def reduce(self): 
+        if type(self.function) == Abstraction:
+            return self.function.body.substitute([self.function.variable, self.argument])
+        else:
+            return self
